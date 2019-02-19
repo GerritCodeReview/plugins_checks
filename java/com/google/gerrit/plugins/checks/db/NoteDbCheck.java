53f1e858055f877b2ba94c1d0e01003af0ee5e24
Patrick Hiesel <hiesel@google.com>
2019-02-19 09:29:32 +0100

    Implement Checks on Revisions
    This commit adds Checks as an entity that Checkers can report on patch
    sets. It adds logic to store the entity in NoteDb, a Java API, a REST
    API, a test API and very basic test coverage.
    
    Checks are stored in Git Notes keyed by the SHA1 of the patch set they
    belong to. NotesMap is used to read from and write to NoteDb.
    
    This change leaves a couple of TODOs for future changes:
    1) The test coverage is *very* basic, but the test framework is there,
       so this work can easily be sharded.
    2) The JavaDoc needs some love.
    3) As a performance improvement, we want to write notes individually
       instead of in bulk. The same applies to reading.
    4) Smaller implementation nits here and there where we throw an
       UnsupportedOperationException or left a TODO.
    5) A cache for Checks and Check status.
    6) Checking that the provided checkerUUIDs match actualy Checkers
    7) Permissions to prevent Checkers from updating other Checkers Checks'
    
    Change-Id: I5109764e2ba776c6e1dcc630559b2ef1a40434ac

---
+++ java/com/google/gerrit/plugins/checks/db/NoteDbCheck.java
@@ -0,0 +1,79 @@
+package com.google.gerrit.plugins.checks.db;
+
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.plugins.checks.Check;
+import com.google.gerrit.plugins.checks.CheckKey;
+import com.google.gerrit.plugins.checks.CheckUpdate;
+import com.google.gerrit.plugins.checks.api.CheckState;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+/** Representation of {@link Check} that can be serialized with GSON. */
+class NoteDbCheck {
+
+  private NoteDbCheck() {}
+
+  public CheckState state;
+  @Nullable public String url;
+  @Nullable public Timestamp started;
+  @Nullable public Timestamp finished;
+
+  public Timestamp created;
+  public Timestamp updated;
+
+  Check toCheck(CheckKey key) {
+    {
+      Check.Builder newCheck =
+          Check.builder(key).setState(state).setCreated(created).setUpdated(updated);
+      if (url != null) {
+        newCheck.setUrl(url);
+      }
+      if (started != null) {
+        newCheck.setStarted(started);
+      }
+      if (finished != null) {
+        newCheck.setFinished(finished);
+      }
+      return newCheck.build();
+    }
+  }
+
+  Check toCheck(Project.NameKey project, PatchSet.Id patchSetId, String checkerUuid) {
+    CheckKey key = CheckKey.create(project, patchSetId, checkerUuid);
+    return toCheck(key);
+  }
+
+  static NoteDbCheck fromCheckUpdate(CheckUpdate checkUpdate) {
+    NoteDbCheck noteDbCheck = new NoteDbCheck();
+    noteDbCheck.applyUpdate(checkUpdate);
+    return noteDbCheck;
+  }
+
+  /**
+   * Applies the given update and returns {@code true} if at least a single fields value was changed
+   * to a different value, {@code false} otherwise. Does not update timestamps.
+   */
+  boolean applyUpdate(CheckUpdate update) {
+    boolean modified = false;
+    if (update.state().isPresent() && !Objects.equals(update.state().orElse(null), state)) {
+      state = update.state().get();
+      modified = true;
+    }
+    if (update.url().isPresent() && !Objects.equals(update.url().orElse(null), url)) {
+      url = update.url().get();
+      modified = true;
+    }
+    if (update.started().isPresent() && !Objects.equals(update.started().orElse(null), started)) {
+      started = update.started().get();
+      modified = true;
+    }
+    if (update.finished().isPresent()
+        && !Objects.equals(update.finished().orElse(null), finished)) {
+      finished = update.finished().get();
+      modified = true;
+    }
+    return modified;
+  }
+}
