GerritCodeReview/gerrit

Implement Checks on Revisions
This commit adds Checks as an entity that Checkers can report on patch
sets. It adds logic to store the entity in NoteDb, a Java API, a REST
API, a test API and very basic test coverage.

Checks are stored in Git Notes keyed by the SHA1 of the patch set they
belong to. NotesMap is used to read from and write to NoteDb.

This change leaves a couple of TODOs for future changes:
1) The test coverage is *very* basic, but the test framework is there,
   so this work can easily be sharded.
2) The JavaDoc needs some love.
3) As a performance improvement, we want to write notes individually
   instead of in bulk. The same applies to reading.
4) Smaller implementation nits here and there where we throw an
   UnsupportedOperationException or left a TODO.
5) A cache for Checks and Check status.
6) Checking that the provided checkerUUIDs match actualy Checkers
7) Permissions to prevent Checkers from updating other Checkers Checks'

Change-Id: I5109764e2ba776c6e1dcc630559b2ef1a40434ac

java/com/google/gerrit/plugins/checkers/acceptance/TestModule.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.plugins.checkers.acceptance;
 
 import com.google.gerrit.plugins.checkers.Module;
+import com.google.gerrit.plugins.checkers.acceptance.testsuite.CheckOperations;
+import com.google.gerrit.plugins.checkers.acceptance.testsuite.CheckOperationsImpl;
 import com.google.gerrit.plugins.checkers.acceptance.testsuite.CheckerOperations;
 import com.google.gerrit.plugins.checkers.acceptance.testsuite.CheckerOperationsImpl;
 import com.google.inject.AbstractModule;
@@ -27,5 +29,6 @@ public class TestModule extends AbstractModule {
     // Only add bindings here that are specifically required for tests, in order to keep the Guice
     // setup in tests as realistic as possible by delegating to the original module.
     bind(CheckerOperations.class).to(CheckerOperationsImpl.class);
+    bind(CheckOperations.class).to(CheckOperationsImpl.class);
   }
 }

java/com/google/gerrit/plugins/checkers/acceptance/testsuite/TestCheckKey.java
@@ -1,0 +1,33 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.acceptance.testsuite;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+
+@AutoValue
+public abstract class TestCheckKey {
+  public abstract Project.NameKey project();
+
+  public abstract PatchSet.Id patchSet();
+
+  public abstract String checkerUUID();
+
+  public static TestCheckKey create(
+      Project.NameKey project, PatchSet.Id patchSet, String checkerUUID) {
+    return new AutoValue_TestCheckKey(project, patchSet, checkerUUID);
+  }
+}

java/com/google/gerrit/plugins/checkers/api/CheckApiImpl.java
@@ -1,0 +1,99 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.api;
+
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.plugins.checkers.Check;
+import com.google.gerrit.plugins.checkers.CheckJson;
+import com.google.gerrit.plugins.checkers.CheckKey;
+import com.google.gerrit.plugins.checkers.CheckUpdate;
+import com.google.gerrit.plugins.checkers.Checks;
+import com.google.gerrit.plugins.checkers.ChecksUpdate;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.UserInitiated;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.assistedinject.Assisted;
+import java.io.IOException;
+import java.util.Optional;
+
+public class CheckApiImpl implements CheckApi {
+  public interface Factory {
+    CheckApiImpl create(CheckResource c);
+  }
+
+  private final Checks checks;
+  private final CheckJson checkJson;
+  private final Provider<ChecksUpdate> checksUpdate;
+  private final CheckResource checkResource;
+
+  @Inject
+  CheckApiImpl(
+      Checks checks,
+      CheckJson checkJson,
+      @UserInitiated Provider<ChecksUpdate> checksUpdate,
+      @Assisted CheckResource checkResource) {
+    this.checks = checks;
+    this.checkJson = checkJson;
+    this.checksUpdate = checksUpdate;
+    this.checkResource = checkResource;
+  }
+
+  @Override
+  public CheckInfo get() throws RestApiException {
+    return checkJson.format(checkResource.getCheck());
+  }
+
+  @Override
+  public CheckInfo update(CheckInput input) throws RestApiException, IOException, OrmException {
+    if (input == null) {
+      throw new BadRequestException("input is required");
+    }
+    if (input.checkerUUID != null && !input.checkerUUID.equals(checkResource.getCheckerUUID())) {
+      throw new BadRequestException(
+          "checkerUUID must either be null or the same as on the resource");
+    }
+
+    Project.NameKey project = checkResource.getRevisionResource().getProject();
+
+    CheckKey key =
+        CheckKey.create(
+            project,
+            checkResource.getRevisionResource().getPatchSet().getId(),
+            checkResource.getCheckerUUID());
+    Optional<Check> check = checks.getCheck(key);
+    if (!check.isPresent()) {
+      throw new ResourceNotFoundException("Not found: " + input.checkerUUID);
+    }
+
+    checksUpdate.get().updateCheck(key, toCheckUpdate(input));
+
+    Optional<Check> updatedCheck = checks.getCheck(key);
+    return checkJson.format(
+        updatedCheck.orElseThrow(() -> new IOException("updated check absent")));
+  }
+
+  private static CheckUpdate toCheckUpdate(CheckInput input) {
+    return CheckUpdate.builder()
+        .setState(Optional.ofNullable(input.state))
+        .setUrl(Optional.ofNullable(input.url))
+        .setStarted(Optional.ofNullable(input.started))
+        .setFinished(Optional.ofNullable(input.finished))
+        .build();
+  }
+}

java/com/google/gerrit/plugins/checkers/db/CheckRevisionNote.java
@@ -1,0 +1,51 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.db;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.gerrit.server.notedb.ChangeNoteJson;
+import com.google.gerrit.server.notedb.RevisionNote;
+import com.google.gson.reflect.TypeToken;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.List;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+
+public class CheckRevisionNote extends RevisionNote<NoteDbCheck> {
+
+  private final ChangeNoteJson noteUtil;
+
+  CheckRevisionNote(ChangeNoteJson noteUtil, ObjectReader reader, ObjectId noteId) {
+    super(reader, noteId);
+    this.noteUtil = noteUtil;
+  }
+
+  @Override
+  protected List<NoteDbCheck> parse(byte[] raw, int offset)
+      throws IOException, ConfigInvalidException {
+    Type listType = new TypeToken<List<NoteDbCheck>>() {}.getType();
+    try (InputStream is = new ByteArrayInputStream(raw, offset, raw.length - offset);
+        Reader r = new InputStreamReader(is, UTF_8)) {
+      return noteUtil.getGson().fromJson(r, listType);
+    }
+  }
+}

javatests/com/google/gerrit/plugins/checkers/acceptance/api/CreateCheckIT.java
@@ -1,0 +1,95 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.acceptance.api;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
+import com.google.gerrit.plugins.checkers.acceptance.AbstractCheckersTest;
+import com.google.gerrit.plugins.checkers.acceptance.testsuite.CheckOperations.PerCheckOperations;
+import com.google.gerrit.plugins.checkers.acceptance.testsuite.TestCheckKey;
+import com.google.gerrit.plugins.checkers.api.CheckInfo;
+import com.google.gerrit.plugins.checkers.api.CheckInput;
+import com.google.gerrit.plugins.checkers.api.CheckState;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.RevId;
+import com.google.gerrit.testing.TestTimeUtil;
+import com.google.inject.Inject;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CreateCheckIT extends AbstractCheckersTest {
+
+  @Inject private ProjectOperations projectOperations;
+
+  private PatchSet.Id patchSetId;
+  private RevId revId;
+
+  @Before
+  public void setTimeForTesting() throws Exception {
+    TestTimeUtil.resetWithClockStep(1, TimeUnit.SECONDS);
+    TestTimeUtil.setClock(Timestamp.from(Instant.EPOCH));
+
+    patchSetId = createChange().getPatchSetId();
+    revId = new RevId(gApi.changes().id(patchSetId.changeId.get()).current().commit(false).commit);
+  }
+
+  @After
+  public void resetTime() {
+    TestTimeUtil.useSystemTime();
+  }
+
+  @Test
+  public void createCheck() throws Exception {
+    String checkerUUID = "my-checker";
+
+    CheckInput input = new CheckInput();
+    input.checkerUUID = checkerUUID;
+    input.state = CheckState.RUNNING;
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).create(input).get();
+    assertThat(info.checkerUUID).isEqualTo(checkerUUID);
+    assertThat(info.state).isEqualTo(CheckState.RUNNING);
+    assertThat(info.started).isNull();
+    assertThat(info.finished).isNull();
+    assertThat(info.created).isNotNull();
+    assertThat(info.updated).isNotNull();
+
+    TestCheckKey key = TestCheckKey.create(project, patchSetId, checkerUUID);
+    PerCheckOperations perCheckOps = checkOperations.check(key);
+
+    // TODO(gerrit-team) Add a Truth subject for the notes map
+    Map<RevId, String> notes = perCheckOps.notesAsText();
+    assertThat(notes).containsEntry(revId, noteDbContent());
+  }
+
+  // TODO(gerrit-team) More tests, especially for multiple checkers and PS and how commits behave
+
+  private String noteDbContent() {
+    return "[\n"
+        + "  {\n"
+        + "    \"checkerUUID\": \"my-checker\",\n"
+        + "    \"state\": \"RUNNING\",\n"
+        + "    \"created\": \"1970-01-01T00:00:23Z\",\n"
+        + "    \"updated\": \"1970-01-01T00:00:23Z\"\n"
+        + "  }\n"
+        + "]";
+  }
+}

GerritCodeReview/plugins_checks

java/com/google/gerrit/plugins/checks/acceptance/TestModule.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.plugins.checks.acceptance;
 
 import com.google.gerrit.plugins.checks.Module;
+import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckOperations;
+import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckOperationsImpl;
 import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckerOperations;
 import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckerOperationsImpl;
 import com.google.inject.AbstractModule;
@@ -27,5 +29,6 @@ public class TestModule extends AbstractModule {
     // Only add bindings here that are specifically required for tests, in order to keep the Guice
     // setup in tests as realistic as possible by delegating to the original module.
     bind(CheckerOperations.class).to(CheckerOperationsImpl.class);
+    bind(CheckOperations.class).to(CheckOperationsImpl.class);
   }
 }

java/com/google/gerrit/plugins/checks/acceptance/testsuite/TestCheckKey.java
@@ -1,0 +1,33 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.acceptance.testsuite;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+
+@AutoValue
+public abstract class TestCheckKey {
+  public abstract Project.NameKey project();
+
+  public abstract PatchSet.Id patchSet();
+
+  public abstract String checkerUUID();
+
+  public static TestCheckKey create(
+      Project.NameKey project, PatchSet.Id patchSet, String checkerUUID) {
+    return new AutoValue_TestCheckKey(project, patchSet, checkerUUID);
+  }
+}

java/com/google/gerrit/plugins/checks/api/CheckApiImpl.java
@@ -1,0 +1,99 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.api;
+
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.plugins.checks.Check;
+import com.google.gerrit.plugins.checks.CheckJson;
+import com.google.gerrit.plugins.checks.CheckKey;
+import com.google.gerrit.plugins.checks.CheckUpdate;
+import com.google.gerrit.plugins.checks.Checks;
+import com.google.gerrit.plugins.checks.ChecksUpdate;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.UserInitiated;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.assistedinject.Assisted;
+import java.io.IOException;
+import java.util.Optional;
+
+public class CheckApiImpl implements CheckApi {
+  public interface Factory {
+    CheckApiImpl create(CheckResource c);
+  }
+
+  private final Checks checks;
+  private final CheckJson checkJson;
+  private final Provider<ChecksUpdate> checksUpdate;
+  private final CheckResource checkResource;
+
+  @Inject
+  CheckApiImpl(
+      Checks checks,
+      CheckJson checkJson,
+      @UserInitiated Provider<ChecksUpdate> checksUpdate,
+      @Assisted CheckResource checkResource) {
+    this.checks = checks;
+    this.checkJson = checkJson;
+    this.checksUpdate = checksUpdate;
+    this.checkResource = checkResource;
+  }
+
+  @Override
+  public CheckInfo get() throws RestApiException {
+    return checkJson.format(checkResource.getCheck());
+  }
+
+  @Override
+  public CheckInfo update(CheckInput input) throws RestApiException, IOException, OrmException {
+    if (input == null) {
+      throw new BadRequestException("input is required");
+    }
+    if (input.checkerUUID != null && !input.checkerUUID.equals(checkResource.getCheckerUUID())) {
+      throw new BadRequestException(
+          "checkerUUID must either be null or the same as on the resource");
+    }
+
+    Project.NameKey project = checkResource.getRevisionResource().getProject();
+
+    CheckKey key =
+        CheckKey.create(
+            project,
+            checkResource.getRevisionResource().getPatchSet().getId(),
+            checkResource.getCheckerUUID());
+    Optional<Check> check = checks.getCheck(key);
+    if (!check.isPresent()) {
+      throw new ResourceNotFoundException("Not found: " + input.checkerUUID);
+    }
+
+    checksUpdate.get().updateCheck(key, toCheckUpdate(input));
+
+    Optional<Check> updatedCheck = checks.getCheck(key);
+    return checkJson.format(
+        updatedCheck.orElseThrow(() -> new IOException("updated check absent")));
+  }
+
+  private static CheckUpdate toCheckUpdate(CheckInput input) {
+    return CheckUpdate.builder()
+        .setState(Optional.ofNullable(input.state))
+        .setUrl(Optional.ofNullable(input.url))
+        .setStarted(Optional.ofNullable(input.started))
+        .setFinished(Optional.ofNullable(input.finished))
+        .build();
+  }
+}

java/com/google/gerrit/plugins/checks/db/CheckRevisionNote.java
@@ -1,0 +1,51 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.db;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.gerrit.server.notedb.ChangeNoteJson;
+import com.google.gerrit.server.notedb.RevisionNote;
+import com.google.gson.reflect.TypeToken;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.List;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+
+public class CheckRevisionNote extends RevisionNote<NoteDbCheck> {
+
+  private final ChangeNoteJson noteUtil;
+
+  CheckRevisionNote(ChangeNoteJson noteUtil, ObjectReader reader, ObjectId noteId) {
+    super(reader, noteId);
+    this.noteUtil = noteUtil;
+  }
+
+  @Override
+  protected List<NoteDbCheck> parse(byte[] raw, int offset)
+      throws IOException, ConfigInvalidException {
+    Type listType = new TypeToken<List<NoteDbCheck>>() {}.getType();
+    try (InputStream is = new ByteArrayInputStream(raw, offset, raw.length - offset);
+        Reader r = new InputStreamReader(is, UTF_8)) {
+      return noteUtil.getGson().fromJson(r, listType);
+    }
+  }
+}

javatests/com/google/gerrit/plugins/checks/acceptance/api/CreateCheckIT.java
@@ -1,0 +1,95 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.acceptance.api;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
+import com.google.gerrit.plugins.checks.acceptance.AbstractCheckersTest;
+import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckOperations.PerCheckOperations;
+import com.google.gerrit.plugins.checks.acceptance.testsuite.TestCheckKey;
+import com.google.gerrit.plugins.checks.api.CheckInfo;
+import com.google.gerrit.plugins.checks.api.CheckInput;
+import com.google.gerrit.plugins.checks.api.CheckState;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.RevId;
+import com.google.gerrit.testing.TestTimeUtil;
+import com.google.inject.Inject;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CreateCheckIT extends AbstractCheckersTest {
+
+  @Inject private ProjectOperations projectOperations;
+
+  private PatchSet.Id patchSetId;
+  private RevId revId;
+
+  @Before
+  public void setTimeForTesting() throws Exception {
+    TestTimeUtil.resetWithClockStep(1, TimeUnit.SECONDS);
+    TestTimeUtil.setClock(Timestamp.from(Instant.EPOCH));
+
+    patchSetId = createChange().getPatchSetId();
+    revId = new RevId(gApi.changes().id(patchSetId.changeId.get()).current().commit(false).commit);
+  }
+
+  @After
+  public void resetTime() {
+    TestTimeUtil.useSystemTime();
+  }
+
+  @Test
+  public void createCheck() throws Exception {
+    String checkerUUID = "my-checker";
+
+    CheckInput input = new CheckInput();
+    input.checkerUUID = checkerUUID;
+    input.state = CheckState.RUNNING;
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).create(input).get();
+    assertThat(info.checkerUUID).isEqualTo(checkerUUID);
+    assertThat(info.state).isEqualTo(CheckState.RUNNING);
+    assertThat(info.started).isNull();
+    assertThat(info.finished).isNull();
+    assertThat(info.created).isNotNull();
+    assertThat(info.updated).isNotNull();
+
+    TestCheckKey key = TestCheckKey.create(project, patchSetId, checkerUUID);
+    PerCheckOperations perCheckOps = checkOperations.check(key);
+
+    // TODO(gerrit-team) Add a Truth subject for the notes map
+    Map<RevId, String> notes = perCheckOps.notesAsText();
+    assertThat(notes).containsEntry(revId, noteDbContent());
+  }
+
+  // TODO(gerrit-team) More tests, especially for multiple checkers and PS and how commits behave
+
+  private String noteDbContent() {
+    return "[\n"
+        + "  {\n"
+        + "    \"checkerUUID\": \"my-checker\",\n"
+        + "    \"state\": \"RUNNING\",\n"
+        + "    \"created\": \"1970-01-01T00:00:23Z\",\n"
+        + "    \"updated\": \"1970-01-01T00:00:23Z\"\n"
+        + "  }\n"
+        + "]";
+  }
+}