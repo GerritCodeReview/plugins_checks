GerritCodeReview/gerrit

WIP: Expose check state as plugin-defined field in ChangeInfo
Change-Id: I6ff767d075e62c14c7e0e7ea45bdbf615c2f3207

java/com/google/gerrit/plugins/checkers/Module.java
@@ -16,12 +16,15 @@ package com.google.gerrit.plugins.checkers;
 
 import static com.google.inject.Scopes.SINGLETON;
 
+import com.google.gerrit.extensions.annotations.Exports;
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.plugins.checkers.api.CombinedChangeStateFactory;
 import com.google.gerrit.plugins.checkers.db.NoteDbCheckersModule;
 import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.git.validators.MergeValidationListener;
 import com.google.gerrit.server.git.validators.RefOperationValidationListener;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor.ChangeAttributeFactory;
 
 public class Module extends FactoryModule {
   @Override
@@ -37,5 +40,9 @@ public class Module extends FactoryModule {
     DynamicSet.bind(binder(), RefOperationValidationListener.class)
         .to(CheckerRefOperationValidator.class)
         .in(SINGLETON);
+
+    bind(ChangeAttributeFactory.class)
+        .annotatedWith(Exports.named("combined_change_state"))
+        .to(CombinedChangeStateFactory.class);
   }
 }

java/com/google/gerrit/plugins/checkers/api/CheckPluginDefinedInfo.java
@@ -1,0 +1,50 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.gerrit.extensions.common.PluginDefinedInfo;
+import java.util.Objects;
+
+public class CheckPluginDefinedInfo extends PluginDefinedInfo {
+  public CombinedCheckState combinedCheckState;
+
+  public CheckPluginDefinedInfo(CombinedCheckState combinedCheckState) {
+    this.combinedCheckState = requireNonNull(combinedCheckState);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(combinedCheckState);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof CheckPluginDefinedInfo)) {
+      return false;
+    }
+    CheckPluginDefinedInfo i = (CheckPluginDefinedInfo) o;
+    return Objects.equals(combinedCheckState, i.combinedCheckState);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("combinedCheckState", combinedCheckState)
+        .toString();
+  }
+}

java/com/google/gerrit/plugins/checkers/api/CombinedChangeStateFactory.java
@@ -1,0 +1,30 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.api;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.gerrit.extensions.common.PluginDefinedInfo;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor.ChangeAttributeFactory;
+
+public class CombinedChangeStateFactory implements ChangeAttributeFactory {
+  // TODO(hiesel): Inject cache and fill in implementation.
+
+  @Override
+  public PluginDefinedInfo create(ChangeData cd, ChangeQueryProcessor qp, String pluginName) {
+    return new CheckPluginDefinedInfo(CombinedCheckState.combine(ImmutableListMultimap.of()));
+  }
+}

javatests/com/google/gerrit/plugins/checkers/acceptance/api/CreateCheckerIT.java
@@ -18,21 +18,27 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.server.testing.CommitSubject.assertCommit;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
 import com.google.gerrit.acceptance.testsuite.request.RequestScopeOperations;
+import com.google.gerrit.extensions.common.PluginDefinedInfo;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.plugins.checkers.acceptance.AbstractCheckersTest;
 import com.google.gerrit.plugins.checkers.acceptance.testsuite.CheckerOperations.PerCheckerOperations;
 import com.google.gerrit.plugins.checkers.api.BlockingCondition;
+import com.google.gerrit.plugins.checkers.api.CheckPluginDefinedInfo;
 import com.google.gerrit.plugins.checkers.api.CheckerInfo;
 import com.google.gerrit.plugins.checkers.api.CheckerInput;
 import com.google.gerrit.plugins.checkers.api.CheckerStatus;
+import com.google.gerrit.plugins.checkers.api.CombinedCheckState;
 import com.google.gerrit.plugins.checkers.db.CheckersByRepositoryNotes;
+import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.testing.TestTimeUtil;
 import com.google.inject.Inject;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
@@ -315,5 +321,16 @@ public class CreateCheckerIT extends AbstractCheckersTest {
     exception.expect(AuthException.class);
     exception.expectMessage("administrateCheckers for plugin checkers not permitted");
     checkersApi.create(input);
+  }
+
+  // TODO(hiesel): This method belongs in a class related to checks, not checkers.
+  @Test
+  public void combinedCheckStateField() throws Exception {
+    Change.Id id = createChange().getChange().getId();
+    assertThat(gApi.changes().id(id.get()).get().plugins).isNull();
+    List<PluginDefinedInfo> plugins =
+        Iterables.getOnlyElement(gApi.changes().query(id.toString()).get()).plugins;
+    assertThat(plugins)
+        .containsExactly(new CheckPluginDefinedInfo(CombinedCheckState.NOT_RELEVANT));
   }
 }

GerritCodeReview/plugins_checks

java/com/google/gerrit/plugins/checks/Module.java
@@ -16,12 +16,15 @@ package com.google.gerrit.plugins.checks;
 
 import static com.google.inject.Scopes.SINGLETON;
 
+import com.google.gerrit.extensions.annotations.Exports;
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.plugins.checks.api.CombinedChangeStateFactory;
 import com.google.gerrit.plugins.checks.db.NoteDbCheckersModule;
 import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.git.validators.MergeValidationListener;
 import com.google.gerrit.server.git.validators.RefOperationValidationListener;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor.ChangeAttributeFactory;
 
 public class Module extends FactoryModule {
   @Override
@@ -37,5 +40,9 @@ public class Module extends FactoryModule {
     DynamicSet.bind(binder(), RefOperationValidationListener.class)
         .to(CheckerRefOperationValidator.class)
         .in(SINGLETON);
+
+    bind(ChangeAttributeFactory.class)
+        .annotatedWith(Exports.named("combined_change_state"))
+        .to(CombinedChangeStateFactory.class);
   }
 }

java/com/google/gerrit/plugins/checks/api/CheckPluginDefinedInfo.java
@@ -1,0 +1,50 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.gerrit.extensions.common.PluginDefinedInfo;
+import java.util.Objects;
+
+public class CheckPluginDefinedInfo extends PluginDefinedInfo {
+  public CombinedCheckState combinedCheckState;
+
+  public CheckPluginDefinedInfo(CombinedCheckState combinedCheckState) {
+    this.combinedCheckState = requireNonNull(combinedCheckState);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(combinedCheckState);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof CheckPluginDefinedInfo)) {
+      return false;
+    }
+    CheckPluginDefinedInfo i = (CheckPluginDefinedInfo) o;
+    return Objects.equals(combinedCheckState, i.combinedCheckState);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("combinedCheckState", combinedCheckState)
+        .toString();
+  }
+}

java/com/google/gerrit/plugins/checks/api/CombinedChangeStateFactory.java
@@ -1,0 +1,32 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.api;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.gerrit.extensions.common.PluginDefinedInfo;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor.ChangeAttributeFactory;
+import com.google.inject.Singleton;
+
+@Singleton
+public class CombinedChangeStateFactory implements ChangeAttributeFactory {
+  // TODO(hiesel): Inject cache and fill in implementation.
+
+  @Override
+  public PluginDefinedInfo create(ChangeData cd, ChangeQueryProcessor qp, String pluginName) {
+    return new CheckPluginDefinedInfo(CombinedCheckState.combine(ImmutableListMultimap.of()));
+  }
+}

javatests/com/google/gerrit/plugins/checks/acceptance/api/CreateCheckerIT.java
@@ -18,21 +18,27 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.server.testing.CommitSubject.assertCommit;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
 import com.google.gerrit.acceptance.testsuite.request.RequestScopeOperations;
+import com.google.gerrit.extensions.common.PluginDefinedInfo;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.plugins.checks.acceptance.AbstractCheckersTest;
 import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckerOperations.PerCheckerOperations;
 import com.google.gerrit.plugins.checks.api.BlockingCondition;
+import com.google.gerrit.plugins.checks.api.CheckPluginDefinedInfo;
 import com.google.gerrit.plugins.checks.api.CheckerInfo;
 import com.google.gerrit.plugins.checks.api.CheckerInput;
 import com.google.gerrit.plugins.checks.api.CheckerStatus;
+import com.google.gerrit.plugins.checks.api.CombinedCheckState;
 import com.google.gerrit.plugins.checks.db.CheckersByRepositoryNotes;
+import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.testing.TestTimeUtil;
 import com.google.inject.Inject;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
@@ -315,5 +321,16 @@ public class CreateCheckerIT extends AbstractCheckersTest {
     exception.expect(AuthException.class);
     exception.expectMessage("administrateCheckers for plugin checks not permitted");
     checkersApi.create(input);
+  }
+
+  // TODO(hiesel): This method belongs in a class related to checks, not checkers.
+  @Test
+  public void combinedCheckStateField() throws Exception {
+    Change.Id id = createChange().getChange().getId();
+    assertThat(gApi.changes().id(id.get()).get().plugins).isNull();
+    List<PluginDefinedInfo> plugins =
+        Iterables.getOnlyElement(gApi.changes().query(id.toString()).get()).plugins;
+    assertThat(plugins)
+        .containsExactly(new CheckPluginDefinedInfo(CombinedCheckState.NOT_RELEVANT));
   }
 }