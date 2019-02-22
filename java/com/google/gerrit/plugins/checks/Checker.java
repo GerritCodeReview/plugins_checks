GerritCodeReview/gerrit

Allow setting blocking conditions on checkers
There are several use cases for providing slightly different behavior in
terms of blocking submission of a change when a checker fails. The most
obvious case is the one implemented in this change: a checker may be
configured to block submission when its check has not yet passed. With a
little imagination we can come up with more cases, such as ignoring
crashed checkers. So we don't want this to be a simple "required" bit.
On the other hand, taken to the extreme, we could support some kind of
arbitrary user-provided code to determine submittability. Our experience
with Prolog submit rules tells us that we do not want anything that
complicated[1].

Go one small step beyond a "required" bit and support a set of
conditions which are defined in a new BlockingCondition enum. This
allows for modest future extension within a simple interface.

[1] Plus, anybody who *really* cares could in theory write a submit rule
    plugin that inspects the checker results and does any computation
    they want.

Change-Id: I9fc198c5fceba0c5fc3df0760c614a81f58ac094

java/com/google/gerrit/server/checker/Checker.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.server.checker;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.gerrit.extensions.api.checkers.BlockingCondition;
 import com.google.gerrit.extensions.api.checkers.CheckerStatus;
 import com.google.gerrit.reviewdb.client.Project;
 import java.sql.Timestamp;
@@ -78,6 +80,13 @@ public abstract class Checker {
   public abstract CheckerStatus getStatus();
 
   /**
+   * Returns the blocking conditions for the checker.
+   *
+   * @return the blocking conditions for the checker.
+   */
+  public abstract ImmutableSortedSet<BlockingCondition> getBlockingConditions();
+
+  /**
    * Returns the creation timestamp of the checker.
    *
    * @return the creation timestamp
@@ -118,6 +127,8 @@ public abstract class Checker {
     public abstract Builder setRepository(Project.NameKey repository);
 
     public abstract Builder setStatus(CheckerStatus status);
+
+    public abstract Builder setBlockingConditions(Iterable<BlockingCondition> enumList);
 
     public abstract Builder setCreatedOn(Timestamp createdOn);
 

java/com/google/gerrit/server/checker/CheckerUpdate.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.server.checker;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.gerrit.extensions.api.checkers.BlockingCondition;
 import com.google.gerrit.extensions.api.checkers.CheckerStatus;
 import com.google.gerrit.reviewdb.client.Project;
 import java.sql.Timestamp;
@@ -50,6 +52,12 @@ public abstract class CheckerUpdate {
   public abstract Optional<CheckerStatus> getStatus();
 
   /**
+   * Defines the new blocking conditions for the checker. Specifies the entire set, not a delta. If
+   * not specified, the blocking conditions remain unchanged.
+   */
+  public abstract Optional<ImmutableSortedSet<BlockingCondition>> getBlockingConditions();
+
+  /**
    * Defines the {@code Timestamp} to be used for the NoteDb commits of the update. If not
    * specified, the current {@code Timestamp} when creating the commit will be used.
    *
@@ -76,6 +84,8 @@ public abstract class CheckerUpdate {
     public abstract Builder setRepository(Project.NameKey repository);
 
     public abstract Builder setStatus(CheckerStatus status);
+
+    public abstract Builder setBlockingConditions(ImmutableSortedSet<BlockingCondition> blockingConditions);
 
     public abstract Builder setUpdatedOn(Timestamp timestamp);
 

java/com/google/gerrit/server/checker/db/CheckerConfigEntry.java
@@ -14,14 +14,28 @@
 
 package com.google.gerrit.server.checker.db;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSortedSet.toImmutableSortedSet;
+import static java.util.Comparator.naturalOrder;
+
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Streams;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.extensions.api.checkers.BlockingCondition;
 import com.google.gerrit.extensions.api.checkers.CheckerStatus;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.checker.Checker;
 import com.google.gerrit.server.checker.CheckerCreation;
 import com.google.gerrit.server.checker.CheckerUpdate;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Locale;
 import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.util.StringUtils;
 
 /**
  * A basic property of a checker.
@@ -186,7 +200,66 @@ enum CheckerConfigEntry {
           .getStatus()
           .ifPresent(status -> config.setEnum(SECTION_NAME, null, super.keyName, status));
     }
+  },
+
+  BLOCKING_CONDITIONS("blocking") {
+    @Override
+    void readFromConfig(String checkerUuid, Checker.Builder checker, Config config) {
+      checker.setBlockingConditions(
+          getEnumSet(config, SECTION_NAME, null, super.keyName, BlockingCondition.values()));
+    }
+
+    @Override
+    void initNewConfig(Config config, CheckerCreation checkerCreation) {
+      // Do nothing. Blocking conditions will be set by updateConfigValue.
+    }
+
+    @Override
+    void updateConfigValue(Config config, CheckerUpdate checkerUpdate) {
+      checkerUpdate
+          .getBlockingConditions()
+          .ifPresent(
+              blockingConditions ->
+                  setEnumList(config, SECTION_NAME, null, super.keyName, blockingConditions));
+    }
   };
+
+  private static <T extends Enum<T>> ImmutableSortedSet<T> getEnumSet(
+      Config config, String section, @Nullable String subsection, String name, T[] all) {
+    return Arrays.stream(config.getStringList(section, subsection, name))
+        .map(v -> resolveEnum(section, subsection, name, v, all))
+        .collect(toImmutableSortedSet(naturalOrder()));
+  }
+
+  private static <T extends Enum<T>> T resolveEnum(
+      String section, @Nullable String subsection, String name, String value, T[] all) {
+    // Match some resolution semantics of DefaultTypedConfigGetter#getEnum.
+    // TODO(dborowitz): Sure would be nice if Config exposed this logic (or getEnumList) so we
+    // didn't have to replicate it.
+    value = value.replace(' ', '_');
+    for (T e : all) {
+      if (StringUtils.equalsIgnoreCase(e.name(), value)) {
+        return e;
+      }
+    }
+    if (subsection != null) {
+      throw new IllegalArgumentException(
+          MessageFormat.format(
+              JGitText.get().enumValueNotSupported3, section, subsection, name, value));
+    }
+    throw new IllegalArgumentException(
+        MessageFormat.format(JGitText.get().enumValueNotSupported2, section, name, value));
+  }
+
+  private static <T extends Enum<T>> void setEnumList(
+      Config config, String section, @Nullable String subsection, String name, Iterable<T> values) {
+    // Match semantics of Config#setEnum.
+    ImmutableList<String> strings =
+        Streams.stream(values)
+            .map(v -> v.name().toLowerCase(Locale.ROOT).replace('_', ' '))
+            .collect(toImmutableList());
+    config.setStringList(section, subsection, name, strings);
+  }
 
   private static final String SECTION_NAME = "checker";
 

javatests/com/google/gerrit/acceptance/api/checker/UpdateCheckerIT.java
@@ -17,6 +17,7 @@ package com.google.gerrit.acceptance.api.checker;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.server.testing.CommitSubject.assertCommit;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.SkipProjectClone;
@@ -25,6 +26,7 @@ import com.google.gerrit.acceptance.testsuite.checker.CheckerOperations.PerCheck
 import com.google.gerrit.acceptance.testsuite.checker.TestChecker;
 import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
 import com.google.gerrit.acceptance.testsuite.request.RequestScopeOperations;
+import com.google.gerrit.extensions.api.checkers.BlockingCondition;
 import com.google.gerrit.extensions.api.checkers.CheckerInfo;
 import com.google.gerrit.extensions.api.checkers.CheckerInput;
 import com.google.gerrit.extensions.api.checkers.CheckerStatus;
@@ -423,6 +425,20 @@ public class UpdateCheckerIT extends AbstractDaemonTest {
     assertThat(info.status).isEqualTo(CheckerStatus.ENABLED);
     assertThat(checkerOperations.checkersOf(allProjects)).isEmpty();
     assertThat(checkerOperations.checkersOf(repositoryName)).containsExactly(checkerUuid);
+  }
+
+  @Test
+  public void updateCheckerWithBlockingConditions() throws Exception {
+    String checkerUuid =
+        checkerOperations.newChecker().name("my-checker").repository(allProjects).create();
+
+    CheckerInput input = new CheckerInput();
+    input.name = "my-checker";
+    input.repository = allProjects.get();
+    input.blockingConditions = ImmutableSet.of(BlockingCondition.STATE_NOT_PASSING);
+
+    CheckerInfo info = gApi.checkers().id(checkerUuid).update(input);
+    assertThat(info.blockingConditions).containsExactly(BlockingCondition.STATE_NOT_PASSING);
   }
 
   @Test

GerritCodeReview/plugins_checks

java/com/google/gerrit/plugins/checks/Checker.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.plugins.checks;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.gerrit.plugins.checks.api.BlockingCondition;
 import com.google.gerrit.plugins.checks.api.CheckerStatus;
 import com.google.gerrit.reviewdb.client.Project;
 import java.sql.Timestamp;
@@ -78,6 +80,13 @@ public abstract class Checker {
   public abstract CheckerStatus getStatus();
 
   /**
+   * Returns the blocking conditions for the checker.
+   *
+   * @return the blocking conditions for the checker.
+   */
+  public abstract ImmutableSortedSet<BlockingCondition> getBlockingConditions();
+
+  /**
    * Returns the creation timestamp of the checker.
    *
    * @return the creation timestamp
@@ -118,6 +127,8 @@ public abstract class Checker {
     public abstract Builder setRepository(Project.NameKey repository);
 
     public abstract Builder setStatus(CheckerStatus status);
+
+    public abstract Builder setBlockingConditions(Iterable<BlockingCondition> enumList);
 
     public abstract Builder setCreatedOn(Timestamp createdOn);
 

java/com/google/gerrit/plugins/checks/CheckerUpdate.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.plugins.checks;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.gerrit.plugins.checks.api.BlockingCondition;
 import com.google.gerrit.plugins.checks.api.CheckerStatus;
 import com.google.gerrit.reviewdb.client.Project;
 import java.sql.Timestamp;
@@ -50,6 +52,12 @@ public abstract class CheckerUpdate {
   public abstract Optional<CheckerStatus> getStatus();
 
   /**
+   * Defines the new blocking conditions for the checker. Specifies the entire set, not a delta. If
+   * not specified, the blocking conditions remain unchanged.
+   */
+  public abstract Optional<ImmutableSortedSet<BlockingCondition>> getBlockingConditions();
+
+  /**
    * Defines the {@code Timestamp} to be used for the NoteDb commits of the update. If not
    * specified, the current {@code Timestamp} when creating the commit will be used.
    *
@@ -76,6 +84,9 @@ public abstract class CheckerUpdate {
     public abstract Builder setRepository(Project.NameKey repository);
 
     public abstract Builder setStatus(CheckerStatus status);
+
+    public abstract Builder setBlockingConditions(
+        ImmutableSortedSet<BlockingCondition> blockingConditions);
 
     public abstract Builder setUpdatedOn(Timestamp timestamp);
 

java/com/google/gerrit/plugins/checks/db/CheckerConfigEntry.java
@@ -14,14 +14,28 @@
 
 package com.google.gerrit.plugins.checks.db;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSortedSet.toImmutableSortedSet;
+import static java.util.Comparator.naturalOrder;
+
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Streams;
+import com.google.gerrit.common.Nullable;
 import com.google.gerrit.plugins.checks.Checker;
 import com.google.gerrit.plugins.checks.CheckerCreation;
 import com.google.gerrit.plugins.checks.CheckerUpdate;
+import com.google.gerrit.plugins.checks.api.BlockingCondition;
 import com.google.gerrit.plugins.checks.api.CheckerStatus;
 import com.google.gerrit.reviewdb.client.Project;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Locale;
 import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.util.StringUtils;
 
 /**
  * A basic property of a checker.
@@ -186,7 +200,66 @@ enum CheckerConfigEntry {
           .getStatus()
           .ifPresent(status -> config.setEnum(SECTION_NAME, null, super.keyName, status));
     }
+  },
+
+  BLOCKING_CONDITIONS("blocking") {
+    @Override
+    void readFromConfig(String checkerUuid, Checker.Builder checker, Config config) {
+      checker.setBlockingConditions(
+          getEnumSet(config, SECTION_NAME, null, super.keyName, BlockingCondition.values()));
+    }
+
+    @Override
+    void initNewConfig(Config config, CheckerCreation checkerCreation) {
+      // Do nothing. Blocking conditions will be set by updateConfigValue.
+    }
+
+    @Override
+    void updateConfigValue(Config config, CheckerUpdate checkerUpdate) {
+      checkerUpdate
+          .getBlockingConditions()
+          .ifPresent(
+              blockingConditions ->
+                  setEnumList(config, SECTION_NAME, null, super.keyName, blockingConditions));
+    }
   };
+
+  private static <T extends Enum<T>> ImmutableSortedSet<T> getEnumSet(
+      Config config, String section, @Nullable String subsection, String name, T[] all) {
+    return Arrays.stream(config.getStringList(section, subsection, name))
+        .map(v -> resolveEnum(section, subsection, name, v, all))
+        .collect(toImmutableSortedSet(naturalOrder()));
+  }
+
+  private static <T extends Enum<T>> T resolveEnum(
+      String section, @Nullable String subsection, String name, String value, T[] all) {
+    // Match some resolution semantics of DefaultTypedConfigGetter#getEnum.
+    // TODO(dborowitz): Sure would be nice if Config exposed this logic (or getEnumList) so we
+    // didn't have to replicate it.
+    value = value.replace(' ', '_');
+    for (T e : all) {
+      if (StringUtils.equalsIgnoreCase(e.name(), value)) {
+        return e;
+      }
+    }
+    if (subsection != null) {
+      throw new IllegalArgumentException(
+          MessageFormat.format(
+              JGitText.get().enumValueNotSupported3, section, subsection, name, value));
+    }
+    throw new IllegalArgumentException(
+        MessageFormat.format(JGitText.get().enumValueNotSupported2, section, name, value));
+  }
+
+  private static <T extends Enum<T>> void setEnumList(
+      Config config, String section, @Nullable String subsection, String name, Iterable<T> values) {
+    // Match semantics of Config#setEnum.
+    ImmutableList<String> strings =
+        Streams.stream(values)
+            .map(v -> v.name().toLowerCase(Locale.ROOT).replace('_', ' '))
+            .collect(toImmutableList());
+    config.setStringList(section, subsection, name, strings);
+  }
 
   private static final String SECTION_NAME = "checker";
 

javatests/com/google/gerrit/plugins/checks/acceptance/api/UpdateCheckerIT.java
@@ -17,6 +17,7 @@ package com.google.gerrit.plugins.checks.acceptance.api;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.server.testing.CommitSubject.assertCommit;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.acceptance.SkipProjectClone;
 import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
 import com.google.gerrit.acceptance.testsuite.request.RequestScopeOperations;
@@ -26,6 +27,7 @@ import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.plugins.checks.acceptance.AbstractCheckersTest;
 import com.google.gerrit.plugins.checks.acceptance.testsuite.CheckerOperations.PerCheckerOperations;
 import com.google.gerrit.plugins.checks.acceptance.testsuite.TestChecker;
+import com.google.gerrit.plugins.checks.api.BlockingCondition;
 import com.google.gerrit.plugins.checks.api.CheckerInfo;
 import com.google.gerrit.plugins.checks.api.CheckerInput;
 import com.google.gerrit.plugins.checks.api.CheckerStatus;
@@ -419,6 +421,20 @@ public class UpdateCheckerIT extends AbstractCheckersTest {
     assertThat(info.status).isEqualTo(CheckerStatus.ENABLED);
     assertThat(checkerOperations.checkersOf(allProjects)).isEmpty();
     assertThat(checkerOperations.checkersOf(repositoryName)).containsExactly(checkerUuid);
+  }
+
+  @Test
+  public void updateCheckerWithBlockingConditions() throws Exception {
+    String checkerUuid =
+        checkerOperations.newChecker().name("my-checker").repository(allProjects).create();
+
+    CheckerInput input = new CheckerInput();
+    input.name = "my-checker";
+    input.repository = allProjects.get();
+    input.blockingConditions = ImmutableSet.of(BlockingCondition.STATE_NOT_PASSING);
+
+    CheckerInfo info = checkersApi.id(checkerUuid).update(input);
+    assertThat(info.blockingConditions).containsExactly(BlockingCondition.STATE_NOT_PASSING);
   }
 
   @Test