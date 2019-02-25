GerritCodeReview/gerrit

Implement Checks on Revisions
This commit adds Checks as an entity that Checkers can report on patch
sets. It adds logic to store the entity in NoteDb, a Java API, a REST
API, a test API and very basic test coverage.

Checks are stored in Git Notes keyed by the SHA1 of the patch set they
belong to. NotesMap is used to read from and write to NoteDb.

This change leaves a couple of TODOs for future changes:
1) The test coverage is *very* basic, but the test framework is there,
   so this work can easily be sharded.
2) The JavaDoc needs some love.
3) As a performance improvement, we want to write notes individually
   instead of in bulk. The same applies to reading.
4) Smaller implementation nits here and there where we throw an
   UnsupportedOperationException or left a TODO.
5) A cache for Checks and Check status.
6) Checking that the provided checkerUUIDs match actualy Checkers
7) Permissions to prevent Checkers from updating other Checkers Checks'

Change-Id: I5109764e2ba776c6e1dcc630559b2ef1a40434ac

java/com/google/gerrit/plugins/checkers/Check.java
@@ -1,0 +1,65 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.plugins.checkers.api.CheckState;
+import java.sql.Timestamp;
+import java.util.Optional;
+
+@AutoValue
+public abstract class Check {
+  /** The key of the Check. */
+  public abstract CheckKey key();
+
+  /** State that this check exited. */
+  public abstract CheckState state();
+  /** Fully qualified URL to detailed result on the Checker's service. */
+  public abstract Optional<String> url();
+  /** Timestamp of when this check was created. */
+  public abstract Optional<Timestamp> started();
+  /** Timestamp of when this check was last updated. */
+  public abstract Optional<Timestamp> finished();
+
+  /** Timestamp of when this check was created. */
+  public abstract Timestamp created();
+  /** Timestamp of when this check was last updated. */
+  public abstract Timestamp updated();
+
+  public abstract Builder toBuilder();
+
+  public static Builder builder(CheckKey key) {
+    return new AutoValue_Check.Builder().setKey(key);
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setKey(CheckKey key);
+
+    public abstract Builder setState(CheckState state);
+
+    public abstract Builder setUrl(String url);
+
+    public abstract Builder setStarted(Timestamp started);
+
+    public abstract Builder setFinished(Timestamp finished);
+
+    public abstract Builder setCreated(Timestamp created);
+
+    public abstract Builder setUpdated(Timestamp updated);
+
+    public abstract Check build();
+  }
+}

java/com/google/gerrit/plugins/checkers/CheckKey.java
@@ -1,0 +1,33 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+
+/** Fields to identify a check. */
+@AutoValue
+public abstract class CheckKey {
+  public abstract Project.NameKey project();
+
+  public abstract PatchSet.Id patchSet();
+
+  public abstract String checkerUUID();
+
+  public static CheckKey create(Project.NameKey project, PatchSet.Id patchSet, String checkerUUID) {
+    return new AutoValue_CheckKey(project, patchSet, checkerUUID);
+  }
+}

java/com/google/gerrit/plugins/checkers/api/ChecksFactory.java
@@ -1,0 +1,67 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.api;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.PatchSetUtil;
+import com.google.gerrit.server.change.ChangeResource;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+/** Factory that instantiates Checks from change and revision resources. Mostly useful for tests. */
+@Singleton
+public class ChecksFactory {
+  private final ChecksImpl.Factory checksFactory;
+  private final PatchSetUtil psUtil;
+  private final ChangeNotes.Factory changeNotesFactory;
+  private final Provider<CurrentUser> user;
+  private final ChangeResource.Factory changeResourceFactory;
+
+  @Inject
+  ChecksFactory(
+      ChecksImpl.Factory checksFactory,
+      PatchSetUtil psUtil,
+      ChangeNotes.Factory changeNotesFactory,
+      ChangeResource.Factory changeResourceFactory,
+      Provider<CurrentUser> user) {
+    this.checksFactory = checksFactory;
+    this.psUtil = psUtil;
+    this.changeNotesFactory = changeNotesFactory;
+    this.user = user;
+    this.changeResourceFactory = changeResourceFactory;
+  }
+
+  public Checks currentRevision(Change.Id changeId) throws OrmException {
+    ChangeNotes notes = changeNotesFactory.createChecked(changeId);
+    PatchSet patchSet = psUtil.current(notes);
+    RevisionResource revisionResource =
+        new RevisionResource(changeResourceFactory.create(notes, user.get()), patchSet);
+    return checksFactory.create(revisionResource);
+  }
+
+  public Checks revision(PatchSet.Id patchSetId) throws OrmException {
+    ChangeNotes notes = changeNotesFactory.createChecked(patchSetId.getParentKey());
+    PatchSet patchSet = psUtil.get(notes, patchSetId);
+    RevisionResource revisionResource =
+        new RevisionResource(changeResourceFactory.create(notes, user.get()), patchSet);
+    return checksFactory.create(revisionResource);
+  }
+}

java/com/google/gerrit/plugins/checkers/api/HttpModule.java
@@ -14,12 +14,15 @@
 
 package com.google.gerrit.plugins.checkers.api;
 
+import static com.google.gerrit.plugins.checkers.api.CheckResource.CHECK_KIND;
 import static com.google.gerrit.plugins.checkers.api.CheckerResource.CHECKER_KIND;
+import static com.google.gerrit.server.change.RevisionResource.REVISION_KIND;
 
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.RestApiModule;
 import com.google.gerrit.httpd.plugins.HttpPluginModule;
+import com.google.gerrit.plugins.checkers.CreateCheck;
 
 public class HttpModule extends HttpPluginModule {
   @Override
@@ -34,10 +37,17 @@ public class HttpModule extends HttpPluginModule {
         new RestApiModule() {
           @Override
           public void configure() {
+            // Checkers
             DynamicMap.mapOf(binder(), CHECKER_KIND);
             postOnCollection(CHECKER_KIND).to(CreateChecker.class);
             get(CHECKER_KIND).to(GetChecker.class);
             post(CHECKER_KIND).to(UpdateChecker.class);
+
+            // Checks
+            DynamicMap.mapOf(binder(), CHECK_KIND);
+            child(REVISION_KIND, "checks").to(ChecksCollection.class);
+            postOnCollection(CHECK_KIND).to(CreateCheck.class);
+            get(CHECK_KIND, "get").to(GetCheck.class);
           }
         });
 
@@ -46,6 +56,8 @@ public class HttpModule extends HttpPluginModule {
           @Override
           public void configure() {
             factory(CheckerApiImpl.Factory.class);
+            factory(ChecksImpl.Factory.class);
+            factory(CheckApiImpl.Factory.class);
           }
         });
   }

java/com/google/gerrit/plugins/checkers/db/NoteDbChecks.java
@@ -1,0 +1,73 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.db;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.gerrit.plugins.checkers.Check;
+import com.google.gerrit.plugins.checkers.CheckKey;
+import com.google.gerrit.plugins.checkers.Checks;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.PatchSetUtil;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+/** Class to read checks from NoteDb. */
+@Singleton
+class NoteDbChecks implements Checks {
+  private final ChangeNotes.Factory changeNotesFactory;
+  private final PatchSetUtil psUtil;
+  private final CheckNotes.Factory checkNotesFactory;
+
+  @Inject
+  NoteDbChecks(
+      ChangeNotes.Factory changeNotesFactory,
+      PatchSetUtil psUtil,
+      CheckNotes.Factory checkNotesFactory) {
+    this.changeNotesFactory = changeNotesFactory;
+    this.psUtil = psUtil;
+    this.checkNotesFactory = checkNotesFactory;
+  }
+
+  @Override
+  public List<Check> getChecks(Project.NameKey projectName, PatchSet.Id psId) throws OrmException {
+    // TODO(gerrit-team): Instead of reading the complete notes map, read just one note.
+    ChangeNotes notes = changeNotesFactory.create(projectName, psId.getParentKey());
+    PatchSet patchSet = psUtil.get(notes, psId);
+    CheckNotes checkNotes = checkNotesFactory.create(notes.getChange());
+    checkNotes.load();
+    return checkNotes
+        .getChecks()
+        .get(patchSet.getRevision())
+        .stream()
+        .map(c -> c.toCheck(projectName, psId))
+        .collect(toImmutableList());
+  }
+
+  @Override
+  public Optional<Check> getCheck(CheckKey checkKey) throws IOException, OrmException {
+    // TODO(gerrit-team): Instead of reading the complete notes map, read just one note.
+    return getChecks(checkKey.project(), checkKey.patchSet())
+        .stream()
+        .filter(c -> c.key().checkerUUID().equals(checkKey.checkerUUID()))
+        .findAny();
+  }
+}

javatests/com/google/gerrit/plugins/checkers/acceptance/GetCheckIT.java
@@ -1,0 +1,58 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.acceptance;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.plugins.checkers.acceptance.testsuite.TestCheckKey;
+import com.google.gerrit.plugins.checkers.api.CheckInfo;
+import com.google.gerrit.plugins.checkers.api.CheckState;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GetCheckIT extends AbstractCheckersTest {
+
+  private PatchSet.Id patchSetId;
+
+  @Before
+  public void setUp() throws Exception {
+    patchSetId = createChange().getPatchSetId();
+  }
+
+  @Test
+  public void getCheck() throws Exception {
+    String name = "my-checker";
+    TestCheckKey key = TestCheckKey.create(project, patchSetId, name);
+    checkOperations.newChecker(key).setState(CheckState.RUNNING).upsert();
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).id(name).get();
+    assertThat(info.checkerUUID).isEqualTo(name);
+    assertThat(info.state).isEqualTo(CheckState.RUNNING);
+    assertThat(info.started).isNull();
+    assertThat(info.finished).isNull();
+    assertThat(info.created).isNotNull();
+    assertThat(info.updated).isNotNull();
+  }
+
+  @Test
+  public void getNonExistingCheckFails() throws Exception {
+    exception.expect(ResourceNotFoundException.class);
+    exception.expectMessage("Not found: non-existing");
+
+    checksApiFactory.revision(patchSetId).id("non-existing").get();
+  }
+}

GerritCodeReview/plugins_checks

java/com/google/gerrit/plugins/checks/Check.java
@@ -1,0 +1,69 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.plugins.checks.api.CheckState;
+import java.sql.Timestamp;
+import java.util.Optional;
+
+@AutoValue
+public abstract class Check {
+  /** The key of the Check. */
+  public abstract CheckKey key();
+
+  /** State that this check is in. */
+  public abstract CheckState state();
+
+  /** Fully qualified URL to detailed result on the Checker's service. */
+  public abstract Optional<String> url();
+
+  /** Timestamp of when this check started. */
+  public abstract Optional<Timestamp> started();
+
+  /** Timestamp of when this check finished. */
+  public abstract Optional<Timestamp> finished();
+
+  /** Timestamp of when this check was created. */
+  public abstract Timestamp created();
+
+  /** Timestamp of when this check was last updated. */
+  public abstract Timestamp updated();
+
+  public abstract Builder toBuilder();
+
+  public static Builder builder(CheckKey key) {
+    return new AutoValue_Check.Builder().setKey(key);
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setKey(CheckKey key);
+
+    public abstract Builder setState(CheckState state);
+
+    public abstract Builder setUrl(String url);
+
+    public abstract Builder setStarted(Timestamp started);
+
+    public abstract Builder setFinished(Timestamp finished);
+
+    public abstract Builder setCreated(Timestamp created);
+
+    public abstract Builder setUpdated(Timestamp updated);
+
+    public abstract Check build();
+  }
+}

java/com/google/gerrit/plugins/checks/CheckKey.java
@@ -1,0 +1,33 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+
+/** Fields to identify a check. */
+@AutoValue
+public abstract class CheckKey {
+  public abstract Project.NameKey project();
+
+  public abstract PatchSet.Id patchSet();
+
+  public abstract String checkerUuid();
+
+  public static CheckKey create(Project.NameKey project, PatchSet.Id patchSet, String checkerUuid) {
+    return new AutoValue_CheckKey(project, patchSet, checkerUuid);
+  }
+}

java/com/google/gerrit/plugins/checks/api/ChecksFactory.java
@@ -1,0 +1,64 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.api;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.PatchSetUtil;
+import com.google.gerrit.server.change.ChangeResource;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+/** Factory that instantiates Checks from change and revision resources. Mostly useful for tests. */
+@Singleton
+public class ChecksFactory {
+  private final ChecksImpl.Factory checksFactory;
+  private final PatchSetUtil psUtil;
+  private final ChangeNotes.Factory changeNotesFactory;
+  private final Provider<CurrentUser> user;
+  private final ChangeResource.Factory changeResourceFactory;
+
+  @Inject
+  ChecksFactory(
+      ChecksImpl.Factory checksFactory,
+      PatchSetUtil psUtil,
+      ChangeNotes.Factory changeNotesFactory,
+      ChangeResource.Factory changeResourceFactory,
+      Provider<CurrentUser> user) {
+    this.checksFactory = checksFactory;
+    this.psUtil = psUtil;
+    this.changeNotesFactory = changeNotesFactory;
+    this.user = user;
+    this.changeResourceFactory = changeResourceFactory;
+  }
+
+  public Checks currentRevision(Change.Id changeId) throws OrmException {
+    ChangeNotes notes = changeNotesFactory.createChecked(changeId);
+    return revision(notes.getChange().currentPatchSetId());
+  }
+
+  public Checks revision(PatchSet.Id patchSetId) throws OrmException {
+    ChangeNotes notes = changeNotesFactory.createChecked(patchSetId.getParentKey());
+    PatchSet patchSet = psUtil.get(notes, patchSetId);
+    RevisionResource revisionResource =
+        new RevisionResource(changeResourceFactory.create(notes, user.get()), patchSet);
+    return checksFactory.create(revisionResource);
+  }
+}

java/com/google/gerrit/plugins/checks/api/HttpModule.java
@@ -14,12 +14,15 @@
 
 package com.google.gerrit.plugins.checks.api;
 
+import static com.google.gerrit.plugins.checks.api.CheckResource.CHECK_KIND;
 import static com.google.gerrit.plugins.checks.api.CheckerResource.CHECKER_KIND;
+import static com.google.gerrit.server.change.RevisionResource.REVISION_KIND;
 
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.RestApiModule;
 import com.google.gerrit.httpd.plugins.HttpPluginModule;
+import com.google.gerrit.plugins.checks.CreateCheck;
 
 public class HttpModule extends HttpPluginModule {
   @Override
@@ -34,10 +37,18 @@ public class HttpModule extends HttpPluginModule {
         new RestApiModule() {
           @Override
           public void configure() {
+            // Checkers
             DynamicMap.mapOf(binder(), CHECKER_KIND);
             postOnCollection(CHECKER_KIND).to(CreateChecker.class);
             get(CHECKER_KIND).to(GetChecker.class);
             post(CHECKER_KIND).to(UpdateChecker.class);
+
+            // Checks
+            // TODO(gerrit-team): add tests in CheckersRestApiBindingsIT
+            DynamicMap.mapOf(binder(), CHECK_KIND);
+            child(REVISION_KIND, "checks").to(ChecksCollection.class);
+            postOnCollection(CHECK_KIND).to(CreateCheck.class);
+            get(CHECK_KIND, "get").to(GetCheck.class);
           }
         });
 
@@ -46,6 +57,8 @@ public class HttpModule extends HttpPluginModule {
           @Override
           public void configure() {
             factory(CheckerApiImpl.Factory.class);
+            factory(ChecksImpl.Factory.class);
+            factory(CheckApiImpl.Factory.class);
           }
         });
   }

java/com/google/gerrit/plugins/checks/db/NoteDbChecks.java
@@ -1,0 +1,76 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.db;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.gerrit.plugins.checks.Check;
+import com.google.gerrit.plugins.checks.CheckKey;
+import com.google.gerrit.plugins.checks.Checks;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.PatchSetUtil;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+/** Class to read checks from NoteDb. */
+@Singleton
+class NoteDbChecks implements Checks {
+  private final ChangeNotes.Factory changeNotesFactory;
+  private final PatchSetUtil psUtil;
+  private final CheckNotes.Factory checkNotesFactory;
+
+  @Inject
+  NoteDbChecks(
+      ChangeNotes.Factory changeNotesFactory,
+      PatchSetUtil psUtil,
+      CheckNotes.Factory checkNotesFactory) {
+    this.changeNotesFactory = changeNotesFactory;
+    this.psUtil = psUtil;
+    this.checkNotesFactory = checkNotesFactory;
+  }
+
+  @Override
+  public List<Check> getChecks(Project.NameKey projectName, PatchSet.Id psId) throws OrmException {
+    // TODO(gerrit-team): Instead of reading the complete notes map, read just one note.
+    ChangeNotes notes = changeNotesFactory.create(projectName, psId.getParentKey());
+    PatchSet patchSet = psUtil.get(notes, psId);
+    CheckNotes checkNotes = checkNotesFactory.create(notes.getChange());
+    checkNotes.load();
+    return checkNotes
+        .getChecks()
+        .get(patchSet.getRevision())
+        .get(0)
+        .checks
+        .entrySet()
+        .stream()
+        .map(e -> e.getValue().toCheck(projectName, psId, e.getKey()))
+        .collect(toImmutableList());
+  }
+
+  @Override
+  public Optional<Check> getCheck(CheckKey checkKey) throws IOException, OrmException {
+    // TODO(gerrit-team): Instead of reading the complete notes map, read just one note.
+    return getChecks(checkKey.project(), checkKey.patchSet())
+        .stream()
+        .filter(c -> c.key().checkerUuid().equals(checkKey.checkerUuid()))
+        .findAny();
+  }
+}

javatests/com/google/gerrit/plugins/checks/acceptance/GetCheckIT.java
@@ -1,0 +1,58 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.acceptance;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.plugins.checks.acceptance.testsuite.TestCheckKey;
+import com.google.gerrit.plugins.checks.api.CheckInfo;
+import com.google.gerrit.plugins.checks.api.CheckState;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GetCheckIT extends AbstractCheckersTest {
+
+  private PatchSet.Id patchSetId;
+
+  @Before
+  public void setUp() throws Exception {
+    patchSetId = createChange().getPatchSetId();
+  }
+
+  @Test
+  public void getCheck() throws Exception {
+    String name = "my-checker";
+    TestCheckKey key = TestCheckKey.create(project, patchSetId, name);
+    checkOperations.newChecker(key).setState(CheckState.RUNNING).upsert();
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).id(name).get();
+    assertThat(info.checkerUuid).isEqualTo(name);
+    assertThat(info.state).isEqualTo(CheckState.RUNNING);
+    assertThat(info.started).isNull();
+    assertThat(info.finished).isNull();
+    assertThat(info.created).isNotNull();
+    assertThat(info.updated).isNotNull();
+  }
+
+  @Test
+  public void getNonExistingCheckFails() throws Exception {
+    exception.expect(ResourceNotFoundException.class);
+    exception.expectMessage("Not found: non-existing");
+
+    checksApiFactory.revision(patchSetId).id("non-existing").get();
+  }
+}