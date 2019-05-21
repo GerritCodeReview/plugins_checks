3648ea6e6b6656a55289dac3fb3f3c728ecd72b6
Ben Rohlfs <brohlfs@google.com>
2019-05-21 20:41:15 +0200

    Enable "Checks" column on dashboard
    Change-Id: I19f34b08fbbd5a9419effe683a3927fabeb4f587

--- gr-checks/gr-checks-change-list-item-cell-view.js
+++ gr-checks/gr-checks-change-list-item-cell-view.js
@@ -1,11 +1,19 @@
 (function() {
-'use strict';
+  'use strict';
 
   Polymer({
     is: 'gr-checks-change-list-item-cell-view',
 
     properties: {
       change: Object,
+    },
+
+    _computeState(change) {
+      if (change && change.plugins && change.plugins.length &&
+          change.plugins[0].combined_state) {
+        return change.plugins[0].combined_state;
+      }
+      return '';
     },
   });
 })();
