d80d88cd0180e6754e4874cb958bd61d8d6027bf
Dhruv Srivastava <dhruvsri@google.com>
2019-06-20 14:44:56 +0200

    Checks table: Order checks according to state and then name
    Change-Id: Ie9f5f059e79f7f6fb627ff2c5a2f165a5cb50d4e

--- gr-checks/gr-checks-view.js
+++ gr-checks/gr-checks-view.js
@@ -2,6 +2,7 @@
   'use strict';
 
   const Defs = {};
+
   /**
    * @typedef {{
    *   _number: number,
@@ -54,6 +55,36 @@
     _fetchChecks(change, revision, getChecks) {
       getChecks(change._number, revision._number).then(checks => {
         if (checks && checks.length) {
+
+          const Statuses = window.Gerrit.Checks.Statuses;
+          const StatusPriorityOrder = [
+            Statuses.FAILED,
+            Statuses.SCHEDULED,
+            Statuses.RUNNING,
+            Statuses.SUCCESSFUL,
+            Statuses.NOT_STARTED,
+            Statuses.NOT_RELEVANT,
+          ];
+          // first sort them by state and then by their name
+          // required failed checks show up before non-required checks
+          checks.sort(
+            (a, b) => {
+              if (a.state != b.state) {
+                let indexA = StatusPriorityOrder.indexOf(a.state);
+                let indexB = StatusPriorityOrder.indexOf(b.state);
+                if (indexA != -1 && indexB != -1) {
+                  return indexA - indexB;
+                }
+                return indexA == -1 ? 1 : -1;
+              }
+              if (a.state === Statuses.FAILED) {
+                if (a.blocking && b.blocking && a.blocking.length !== b.blocking.length) {
+                  return a.blocking.length == 0 ? 1 : -1;
+                }
+              }
+              return a.checker_name.localeCompare(b.checker_name);
+            }
+          )
           this.set('_checks', checks);
           this.set('_status', LoadingStatus.RESULTS);
         } else {
