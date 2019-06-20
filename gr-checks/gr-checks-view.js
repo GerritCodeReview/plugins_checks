40ca90f154364173714b28c6e4946e1e3f5eb459
Dhruv Srivastava <dhruvsri@google.com>
2019-06-20 14:44:56 +0200

    Checks table: Order checks according to state and then name
    Change-Id: Ie9f5f059e79f7f6fb627ff2c5a2f165a5cb50d4e

--- gr-checks/gr-checks-view.js
+++ gr-checks/gr-checks-view.js
@@ -2,6 +2,7 @@
   'use strict';
 
   const Defs = {};
+
   /**
    * @typedef {{
    *   _number: number,
@@ -54,6 +55,43 @@
     _fetchChecks(change, revision, getChecks) {
       getChecks(change._number, revision._number).then(checks => {
         if (checks && checks.length) {
+
+          let Statuses = window.Gerrit.Checks.Statuses;
+          let StatusPriorityOrder = [
+            Statuses.FAILED,
+            Statuses.SCHEDULED,
+            Statuses.RUNNING,
+            Statuses.SUCCESSFUL,
+            Statuses.NOT_STARTED,
+            Statuses.NOT_RELEVANT,
+          ];
+          //add special ordering to checks here
+          //first sort them by state and then by their name
+          checks.sort(
+            (a, b) => {
+              if (a.state != b.state) {
+                //different states so give priority based on state
+                let indexA = StatusPriorityOrder.indexOf(a.state);
+                let indexB = StatusPriorityOrder.indexOf(b.state);
+                if (indexA != -1 && indexB != -1) {
+                  //if both have a valid state then the one with higher priority goes first
+                  return indexA - indexB;
+                }
+                if (indexA == -1) {
+                  //B should go first
+                  return 1;
+                }
+                if (indexB == -1) {
+                  //A should go first
+                  return -1;
+                }
+                //both are invalid so return anything
+                return -1;
+              }
+              //their states are the same so now we sort them based on their name
+              return a.checker_name.localeCompare(b.checker_name);
+            }
+          )
           this.set('_checks', checks);
           this.set('_status', LoadingStatus.RESULTS);
         } else {
