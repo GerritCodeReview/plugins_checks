3a7cd018ec3ccd2b15ddc1abc4b1542f8324d9ce
Dhruv Srivastava <dhruvsri@google.com>
2019-07-10 12:15:08 +0200

    Stop polling for checks if tab goes out of focus
    Change-Id: I999144dcc973ec76b1340ca74183ce651b2a9f4c

--- gr-checks/gr-checks-chip-view.js
+++ gr-checks/gr-checks-chip-view.js
@@ -84,7 +84,8 @@
         type: Boolean,
         value: false
       },
-      pollChecksInterval: Object
+      pollChecksInterval: Object,
+      visibilityChangeListener: Object
     },
 
     observers: [
@@ -124,13 +125,27 @@
       });
     },
 
+    onVisibililityChange(change, revision, getChecks) {
+      if (document.hidden) {
+        clearInterval(this.pollChecksInterval);
+        return;
+      }
+      this._pollChecksRegularly(change, revision, getChecks);
+    },
+
     _pollChecksRegularly(change, revision, getChecks) {
       if (this.pollChecksInterval) {
         clearInterval(this.pollChecksInterval);
       }
       const poll = () => this._fetchChecks(change, revision, getChecks);
       poll();
-      this.pollChecksInterval = setInterval(poll, CHECKS_POLL_INTERVAL_MS)
+      this.pollChecksInterval = setInterval(poll, CHECKS_POLL_INTERVAL_MS);
+      if (!this.visibilityChangeListener) {
+        this.visibilityChangeListener = document.addEventListener(
+          'visibilitychange',
+          this.onVisibililityChange.bind(this, change, revision, getChecks)
+        );
+      }
     },
 
     /**
