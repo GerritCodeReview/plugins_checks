50f70a9e3d3aa71eb88ff6bcf76c46e3e4465550
Dhruv Srivastava <dhruvsri@google.com>
2019-08-08 16:35:36 +0200

    WIP Add UI for configuring checks
    Change-Id: I779aaaf488645f3db56c942496c78c2b7c7ad185

---
+++ gr-checks/gr-checks-list-view.js
@@ -0,0 +1,83 @@
+/**
+ * @license
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function() {
+  'use strict';
+
+  const REQUEST_DEBOUNCE_INTERVAL_MS = 200;
+
+  Polymer({
+    is: 'gr-checks-list-view',
+    _legacyUndefinedCheck: true,
+
+    properties: {
+      createNew: Boolean,
+      items: Array,
+      itemsPerPage: Number,
+      nextClicked: Function,
+      prevClicked: Function,
+      filter: {
+        type: String,
+        observer: '_filterChanged',
+      },
+      offset: Number,
+      loading: Boolean,
+      path: String,
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+      Gerrit.FireBehavior,
+      Gerrit.URLEncodingBehavior,
+    ],
+
+    detached() {
+      this.cancelDebouncer('reload');
+    },
+
+    _filterChanged(filter) {
+      this.fire('filter-changed', {filter});
+    },
+
+    _handlePrevArrowClicked() {
+      this.fire('prev-clicked');
+    },
+
+    _handleNextArrowClicked() {
+      this.fire('next-clicked');
+    },
+
+    _createNewItem() {
+      this.fire('create-clicked');
+    },
+
+    _computeCreateClass(createNew) {
+      return createNew ? 'show' : '';
+    },
+
+    _hidePrevArrow(loading, offset) {
+      return loading || offset === 0;
+    },
+
+    _hideNextArrow(loading, items) {
+      if (loading || !items || !items.length) {
+        return true;
+      }
+      const lastPage = items.length < this.itemsPerPage + 1;
+      return lastPage;
+    },
+  });
+})();
