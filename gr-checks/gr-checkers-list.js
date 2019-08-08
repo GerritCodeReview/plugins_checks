c648cfaace5fe9bd0c7cff553b5f500adedbd3bd
Dhruv Srivastava <dhruvsri@google.com>
2019-08-08 16:35:36 +0200

    Add UI for adding and editing checkers
    * Add a configure button in the checks-view table
    * Show a list of existing checkers upon clicking configure
    * Allow user to create checkers
    * Allow user to edit existing checkers
    
    Change-Id: I779aaaf488645f3db56c942496c78c2b7c7ad185

---
+++ gr-checks/gr-checkers-list.js
@@ -0,0 +1,170 @@
+//show a list of all checkers along with creating/editing them
+(function() {
+  'use strict';
+  const CHECKERS_PER_PAGE = 15;
+  const GET_CHECKERS_URL = "/a/plugins/checks/checkers/";
+  Polymer({
+    is: 'gr-checkers-list',
+    properties: {
+      _checkers: Array,
+      _filteredCheckers: Array,
+      _loading: {
+        type: Boolean,
+        value: true,
+      },
+      _filter: {
+        type: String,
+        value: '',
+      },
+      _visibleCheckers: {
+        type: Array,
+        computed: '_computeVisibleCheckers(_startingIndex, _filteredCheckers)'
+      },
+      _createNewCapability: {
+        type: Boolean,
+        value: true,
+      },
+      _startingIndex: {
+        type: Number,
+        value: 0
+      },
+      pluginRestApi: Object,
+      _handleNextClicked: Function,
+      _handlePrevClicked: Function,
+      _showNextButton: {
+        type: Boolean,
+        value: true,
+        computed: '_computeShowNextButton(_startingIndex, _filteredCheckers)'
+      },
+      _showPrevButton: {
+        type: Boolean,
+        value: true,
+        computed: '_computeShowPrevButton(_startingIndex, _filteredCheckers)'
+      },
+      checker: Object
+    },
+    observers: [
+      '_showCheckers(_checkers, _filter)',
+    ],
+    behaviors: [
+      Gerrit.ListViewBehavior,
+    ],
+
+    attached() {
+      this._getCheckers();
+    },
+
+    _contains(target, keyword) {
+      return target.toLowerCase().includes(keyword.trim());
+    },
+
+    _showCheckers(_checkers, _filter) {
+      if (!_checkers) {
+        return;
+      }
+      if (!_filter) {
+        _filter = '';
+      }
+      //TODO(dhruvsri): highlight matching part
+      this._filteredCheckers = this._checkers.filter(
+        checker => {
+          return this._contains(checker.name, this._filter) || 
+                 this._contains(checker.repository, this._filter);
+        }
+      )
+      this._startingIndex = 0;
+    },
+
+    _computeVisibleCheckers(_startingIndex, _filteredCheckers) {
+      if (!_filteredCheckers) {
+        return [];
+      }
+      return this._filteredCheckers.slice(this._startingIndex, this._startingIndex + CHECKERS_PER_PAGE);
+    },
+
+    _computeShowNextButton(_startingIndex, _filteredCheckers) {
+      if (!_filteredCheckers) {
+        return false;
+      }
+      return (_startingIndex + CHECKERS_PER_PAGE < _filteredCheckers.length)
+    },
+
+    _computeShowPrevButton(_startingIndex, _filteredCheckers) {
+      if (!_filteredCheckers) {
+        return false;
+      }
+      return (_startingIndex >= CHECKERS_PER_PAGE);
+    },
+
+    _handleNextClicked() {
+      if (this._startingIndex + CHECKERS_PER_PAGE < this._filteredCheckers.length) {
+        this._startingIndex += CHECKERS_PER_PAGE;
+      }
+    },
+
+    _handlePrevClicked() {
+      if (this._startingIndex >= CHECKERS_PER_PAGE) {
+        this._startingIndex -= CHECKERS_PER_PAGE;
+      }
+    },
+
+    getCheckers() {
+      return this.pluginRestApi.fetchJSON({
+        method: 'GET',
+        url: GET_CHECKERS_URL,
+      });
+    },
+
+    _getCheckers() {
+      this._checkers = [];
+      return this.getCheckers()
+        .then(checkers => {
+          if (!checkers) { return; }
+          this._checkers = checkers;
+          this._startingIndex = 0;
+          this._loading = false;
+        });
+    },
+
+    _handleEditConfirm() {
+      this.$.editModal._handleEditChecker();
+    },
+
+    _handleEditIconClicked(e) {
+      let checker = e.model.item;
+      this.checker = checker;
+      this.$.editOverlay.open();
+    },
+
+    _handleEditCancel(e) {
+      if (e.detail.reload) {
+        this._getCheckers();
+      }
+      this.$.editOverlay.close();
+    },
+
+    _computeCreateClass(createNew) {
+      return createNew ? 'show' : '';
+    },
+
+    _computeBlocking(checker) {
+      return (checker && checker.blocking && checker.blocking.length > 0)? "YES": "NO";
+    },
+
+    _handleCreateConfirm() {
+      this.$.createNewModal._handleCreateChecker();
+    },
+
+    _handleCreateClicked() {
+      this.$.createOverlay.open();
+    },
+
+    _handleCreateCancel(e) {
+      if (e.detail.reload) {
+        this._getCheckers();
+      }
+      this.$.createOverlay.close();
+    },
+
+  })
+})();
