639d29a0c957d7558ab919cd9daf4e84eb6f0c43
David Ostrovsky <david@ostrovsky.org>
2019-11-19 03:58:11 -0800

    Expose standalone rest api library
    To access the plugin's pending checks REST endpoint from a standalone
    CI system, expose standalone client library that shares the entities
    classes from the api package. Isolate the entities classes in shared
    library so that the classes are built only once. Extending the build
    tool chain to allow to publish the library to Maven Central is outside
    of the scope of this change and might be added in a follow-up change.
    
    Change-Id: I536a6090ad88c9348b22075b367070be72cbfc9b

---
+++ java/com/google/gerrit/plugins/checks/client/Checks.java
@@ -0,0 +1,169 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.client;
+
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.plugins.checks.api.CheckInfo;
+import com.google.gerrit.plugins.checks.api.CheckInput;
+import com.google.gerrit.plugins.checks.api.RerunInput;
+import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.eclipse.jgit.transport.URIish;
+
+public class Checks extends AbstractEndpoint {
+
+  private int changeNumber;
+  private int patchSetNumber;
+
+  public Checks(URIish gerritBaseUrl, CloseableHttpClient client, boolean isAuthenticated)
+      throws URISyntaxException {
+    super(gerritBaseUrl, client, isAuthenticated);
+  }
+
+  public Checks change(int changeNumber) {
+    this.changeNumber = changeNumber;
+    return this;
+  }
+
+  public Checks patchSet(int patchSetNumber) {
+    this.patchSetNumber = patchSetNumber;
+    return this;
+  }
+
+  public CheckInfo create(CheckInput input) throws IOException, RestApiException {
+    try {
+      return performCreateOrUpdate(input);
+    } catch (Exception e) {
+      throw new RestApiException("Could not create check", e);
+    } finally {
+      client.close();
+    }
+  }
+
+  public CheckInfo get(String checkerUuid) throws RestApiException, IOException {
+    try {
+      HttpGet request = new HttpGet(buildRequestUrl(checkerUuid));
+      CloseableHttpResponse response = client.execute(request);
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        return JsonBodyParser.parseResponse(
+            EntityUtils.toString(response.getEntity()), new TypeToken<CheckInfo>() {}.getType());
+      }
+      throw new RestApiException(
+          String.format(
+              "Request failed with status: %d", response.getStatusLine().getStatusCode()));
+    } catch (Exception e) {
+      throw new RestApiException("Failed to get check info: ", e);
+    } finally {
+      client.close();
+    }
+  }
+
+  public List<CheckInfo> list() throws RestApiException, IOException {
+    try {
+      HttpGet request = new HttpGet(buildRequestUrl());
+      CloseableHttpResponse response = client.execute(request);
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        return JsonBodyParser.parseResponse(
+            EntityUtils.toString(response.getEntity()),
+            new TypeToken<List<CheckInfo>>() {}.getType());
+      }
+      throw new RestApiException(
+          String.format(
+              "Request failed with status: %d", response.getStatusLine().getStatusCode()));
+    } catch (Exception e) {
+      throw new RestApiException("Failed to list checks: ", e);
+    } finally {
+      client.close();
+    }
+  }
+
+  public CheckInfo rerun(String checkerUuid) throws RestApiException, IOException {
+    return rerun(checkerUuid, new RerunInput());
+  }
+
+  public CheckInfo rerun(String checkerUuid, RerunInput input)
+      throws RestApiException, IOException {
+    try {
+      HttpPost request = new HttpPost(buildRequestUrl(checkerUuid + "/rerun"));
+      String inputString =
+          JsonBodyParser.createRequestBody(input, new TypeToken<RerunInput>() {}.getType());
+      request.setEntity(new StringEntity(inputString));
+      request.setHeader("Content-type", "application/json");
+      CloseableHttpResponse response = client.execute(request);
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        return JsonBodyParser.parseResponse(
+            EntityUtils.toString(response.getEntity()), new TypeToken<CheckInfo>() {}.getType());
+      }
+      throw new RestApiException(
+          String.format(
+              "Request failed with status: %d", response.getStatusLine().getStatusCode()));
+    } catch (Exception e) {
+      throw new RestApiException("Could not rerun check", e);
+    } finally {
+      client.close();
+    }
+  }
+
+  public CheckInfo update(CheckInput input) throws IOException, RestApiException {
+    try {
+      return performCreateOrUpdate(input);
+    } catch (Exception e) {
+      throw new RestApiException("Could not update check", e);
+    } finally {
+      client.close();
+    }
+  }
+
+  private CheckInfo performCreateOrUpdate(CheckInput input)
+      throws RestApiException, URISyntaxException, ParseException, IOException {
+    HttpPost request = new HttpPost(buildRequestUrl());
+    String inputString =
+        JsonBodyParser.createRequestBody(input, new TypeToken<CheckInput>() {}.getType());
+    request.setEntity(new StringEntity(inputString));
+    request.setHeader("Content-type", "application/json");
+
+    CloseableHttpResponse response = client.execute(request);
+    if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+      return JsonBodyParser.parseResponse(
+          EntityUtils.toString(response.getEntity()), new TypeToken<CheckInfo>() {}.getType());
+    }
+    throw new RestApiException(
+        String.format("Request returned status %s", response.getStatusLine().getStatusCode()));
+  }
+
+  private URI buildRequestUrl() throws URISyntaxException {
+    return buildRequestUrl("");
+  }
+
+  private URI buildRequestUrl(String suffixPath) throws URISyntaxException {
+    return uriBuilder
+        .setPath(
+            String.format(
+                "%schanges/%d/revisions/%d/checks/%s",
+                getPrefix(), changeNumber, patchSetNumber, suffixPath))
+        .build();
+  }
+}
