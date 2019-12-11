e3ccc3475a74773cd11a5f97ae650b496897641e
Dhruv Srivastava <dhruvsri@google.com>
2019-12-11 16:16:57 +0100

    Add patchset navigator for checks table
    Change-Id: I6d29739a68f9b49a98e76017f8ad8aad001fc01c

--- gr-checks/gr-checks-view.js
+++ gr-checks/gr-checks-view.js
@@ -61,20 +61,46 @@
         type: Boolean,
         value: false,
       },
+      allPatchSets: {
+        type: Array,
+        value() { return []; },
+      },
+      _patchSetDropdownItems: {
+        type: Array,
+        value() { return []; },
+      },
+      _currentPatchSet: {
+        type: Number,
+      },
     },
 
     observers: [
-      '_pollChecksRegularly(change, revision, getChecks)',
+      '_pollChecksRegularly(change, _currentPatchSet, getChecks)',
     ],
 
     attached() {
       this.pluginRestApi = this.plugin.restApi();
       this._initCreateCheckerCapability();
+      this._patchSetDropdownItems = this.allPatchSets.filter(patch =>
+        patch.num !== 'edit'
+      ).map(patch => {
+        return {
+          text: 'Patchset ' + patch.num,
+          value: patch.num,
+        };
+      });
+      this._currentPatchSet = this.revision._number;
     },
 
     detached() {
       clearInterval(this.pollChecksInterval);
       this.unlisten(document, 'visibilitychange', '_onVisibililityChange');
+    },
+
+    _handlePatchSetChanged(e) {
+      const patchSet = e.detail.value;
+      if (patchSet === this._currentPatchSet) return;
+      this._currentPatchSet = patchSet;
     },
 
     _handleCheckersListResize() {
@@ -160,10 +186,11 @@
      * @param {!Defs.Revision} revision
      * @param {function(number, number): !Promise<!Object>} getChecks
      */
-    _fetchChecks(change, revision, getChecks) {
-      if (!getChecks || !change || !revision) return;
+    _fetchChecks(change, revisionNumber, getChecks) {
+      if (!getChecks || !change || !revisionNumber) return;
 
-      getChecks(change._number, revision._number).then(checks => {
+      getChecks(change._number, revisionNumber).then(checks => {
+        if (revisionNumber !== this._currentPatchSet) return;
         if (checks && checks.length) {
           checks.sort((a, b) => this._orderChecks(a, b));
           if (!this._checks) {
@@ -183,7 +210,8 @@
         clearInterval(this.pollChecksInterval);
         return;
       }
-      this._pollChecksRegularly(this.change, this.revision, this.getChecks);
+      this._pollChecksRegularly(this.change, this._currentPatchSet,
+          this.getChecks);
     },
 
     _toggleCheckMessage(e) {
@@ -202,11 +230,12 @@
           !this._checks[idx].showCheckMessage);
     },
 
-    _pollChecksRegularly(change, revision, getChecks) {
+    _pollChecksRegularly(change, revisionNumber, getChecks) {
+      if (!change || !revisionNumber || !getChecks) return;
       if (this.pollChecksInterval) {
         clearInterval(this.pollChecksInterval);
       }
-      const poll = () => this._fetchChecks(change, revision, getChecks);
+      const poll = () => this._fetchChecks(change, revisionNumber, getChecks);
       poll();
       this.pollChecksInterval = setInterval(poll, CHECKS_POLL_INTERVAL_MS);
       if (!this.visibilityChangeListenerAdded) {
