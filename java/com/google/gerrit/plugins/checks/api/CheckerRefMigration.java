ce6a2ab1fb77a86d24d66e64c7dc83dc6399b5bc
Gal Paikin <paiking@google.com>
2020-02-28 15:52:12 +0100

    Perform migration from refs/meta/checkers/ to the same ref without slash
    After the previous change corrected the name of the ref, this change has
    to migrate the old ref to be the new ref.
    
    The migration will be done upstream in the InitStep for the checks
    plugin. When upgrading the plugin, Init should be run, and this is
    documented in the init pgm notes.
    
    Alternatively, we could use LifeCycleListener, but then we would have
    to continuously pay the price of opening the repository and checking for
    the ref, so the current approach is cheaper.
    
    Change-Id: Ic5c4e8188b9c6fb05258602063ff9617b44e8791

---
+++ java/com/google/gerrit/plugins/checks/api/CheckerRefMigration.java
@@ -0,0 +1,127 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.plugins.checks.api;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.plugins.checks.CheckerRef;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.update.BatchUpdate;
+import com.google.inject.Provider;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefRename;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.RefUpdate.Result;
+import org.eclipse.jgit.lib.Repository;
+
+@Singleton
+public class CheckerRefMigration {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final GitRepositoryManager repoManager;
+  private final AllProjectsName allProjectsName;
+  private final BatchUpdate.Factory updateFactory;
+  private final Provider<CurrentUser> user;
+
+  private static final String TMP_REF = "refs/tmp/checker-migration";
+
+  @Inject
+  CheckerRefMigration(
+      GitRepositoryManager repoManager,
+      AllProjectsName allProjectsName,
+      BatchUpdate.Factory updateFactory,
+      Provider<CurrentUser> user) {
+    this.repoManager = repoManager;
+    this.allProjectsName = allProjectsName;
+    this.updateFactory = updateFactory;
+    this.user = user;
+  }
+
+  public void migrate() {
+    try (Repository repo = repoManager.openRepository(allProjectsName)) {
+
+      Ref old = repo.exactRef("refs/meta/checkers/");
+      Ref newRef = repo.exactRef("refs/meta/checkers");
+      if (old == null) {
+        // already migrated
+        return;
+      }
+      if (newRef == null) {
+        // Create refs/meta/checkers at the tip of refs/meta/checkers/
+        RefUpdate update = repo.updateRef("refs/meta/checkers");
+        update.setExpectedOldObjectId(ObjectId.zeroId());
+        update.setNewObjectId(old.getObjectId());
+        update.setForceUpdate(true); // not sure if this needs to be forced, please check
+        update.update(); // check update with util
+      }
+      // Delete refs/meta/checkers/
+      RefUpdate update = repo.updateRef("refs/meta/checkers");
+      update.setExpectedOldObjectId(old.getObjectId());
+      update.setNewObjectId(ObjectId.zeroId());
+      update.setForceUpdate(true); // not sure if this needs to be forced, please check
+      update.update(); // check update with util
+    } catch (Exception ex) {
+      // ignore, if failed for any reason it shouldn't stop the plugin from loading.
+      logger.atWarning().withCause(ex).log(
+          "Failed to migrate refs/meta/checkers/ to refs/meta/checkers (without trailing slash");
+    }
+  }
+
+  public void migrateOld() {
+    try (Repository repo = repoManager.openRepository(allProjectsName)) {
+
+      // This part is specifically for cases where the rename failed half-way last time.
+      Ref ref = repo.exactRef(TMP_REF);
+      if (ref != null) {
+        renameRef(TMP_REF, CheckerRef.REFS_META_CHECKERS, repo);
+        return;
+      }
+
+      ref = repo.exactRef(CheckerRef.LEGACY_REFS_META_CHECKERS);
+      if (ref == null) {
+        return;
+      }
+      renameRef(CheckerRef.LEGACY_REFS_META_CHECKERS, TMP_REF, repo);
+      renameRef(TMP_REF, CheckerRef.REFS_META_CHECKERS, repo);
+    } catch (Exception ex) {
+      // ignore, if failed for any reason it shouldn't stop the plugin from loading.
+      logger.atWarning().withCause(ex).log(
+          "Failed to migrate refs/meta/checkers/ to refs/meta/checkers (without trailing slash");
+    }
+  }
+
+  private void renameRef(String currentName, String targetName, Repository repo)
+      throws IOException, RestApiException {
+    RefRename refRename = repo.renameRef(currentName, targetName);
+    Result result = refRename.rename();
+    if (result != Result.RENAMED) {
+      throw new ResourceConflictException(
+          String.format(
+              "Rename of %s to %s failed with the failure: %s",
+              currentName, targetName, result.name()));
+    }
+    RefUpdate update = repo.updateRef(currentName);
+    update.setExpectedOldObjectId(repo.exactRef(currentName).getObjectId());
+    update.setNewObjectId(ObjectId.zeroId());
+    update.setForceUpdate(true);
+    update.delete();
+  }
+}
