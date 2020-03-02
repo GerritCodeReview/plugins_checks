6a6b3ca4e4fdb1ab7f10ba64783410690c139a8d
Gal Paikin <paiking@google.com>
2020-03-02 13:46:28 +0100

    Create OverridePolicy interface
    Create the interface that will be used to calculate whether or not a
    check has been overridden.
    
    Change-Id: I29ef084a3ecfed5bf1a7c58517741d04806bf578

---
+++ java/com/google/gerrit/plugins/checks/api/OverrideImpact.java
@@ -0,0 +1,26 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.api;
+
+import java.util.Optional;
+
+public class OverrideImpact {
+
+  // True if the check has been overridden. False otherwise.
+  public boolean overridden;
+
+  // A possibly custom message about the overrides.
+  public Optional<String> message;
+}
