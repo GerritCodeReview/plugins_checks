8dec64768100a7a62c4be40a456b6245fade6777
Paladox none <thomasmulhall410@yahoo.com>
2020-05-13 18:06:02 +0000

    Change htmlTemplate to be a var
    Using const for htmlTemplate seems to be conflicting with
    delete-project version.
    
    Change-Id: I01daf0a190f30bb49a3780178310f3caf1d23bf3

--- gr-checks/gr-checks-status_html.js
+++ gr-checks/gr-checks-status_html.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-export const htmlTemplate = Polymer.html`
+export var htmlTemplate = Polymer.html`
       <style>
         :host {
           display: inline-block;
