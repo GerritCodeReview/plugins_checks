2d74049f03e716aeef04f876b651cb9cb716b3ac
Ben Rohlfs <brohlfs@google.com>
2021-04-27 15:18:39 +0200

    Add a popup for configuring checks as a top-level action
    Bug: Issue 14336
    Change-Id: I09ac406277f1f78c76b6263c9f4f86bb68445c4f

--- gr-checks/plugin.js
+++ gr-checks/plugin.js
@@ -14,11 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import './gr-checkers-list.js';
 import {computeDuration} from './util.js';
 
 class ChecksFetcher {
-  constructor(restApi) {
-    this.restApi = restApi;
+  constructor(plugin) {
+    this.plugin = plugin;
+    this.restApi = plugin.restApi();
   }
 
   async fetchCurrent() {
@@ -32,6 +34,10 @@ class ChecksFetcher {
     const checks = await this.apiGet('?o=CHECKER');
     return {
       responseCode: 'OK',
+      actions: [{
+        name: 'Configure Checkers',
+        callback: () => this.plugin.popup('gr-checkers-list'),
+      }],
       runs: checks.map(check => this.convert(check)),
     };
   }
@@ -109,8 +115,14 @@ class ChecksFetcher {
 }
 
 Gerrit.install(plugin => {
+  // We need to somehow pass on the `plugin` object to the popup of
+  // <gr-checkers-list>. Using a global window variable is obviously a hacky
+  // workaround. The best way to fix this properly is probably to change
+  // the plugin popup to retrieve the `plugin` object from the
+  // 'plugin-overlay' endpoint and then pass it on to all child elements.
+  window.__gerrit_checks_plugin = plugin;
   const checksApi = plugin.checks();
-  const fetcher = new ChecksFetcher(plugin.restApi());
+  const fetcher = new ChecksFetcher(plugin);
   checksApi.register({
     fetch: data => fetcher.fetch(data),
   });
